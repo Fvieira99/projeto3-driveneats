ec095069e990b66a291b4b9f6e32f7a12b7b1c9f
Felipe <felipecosta1961@gmail.com>
2022-01-26 01:49:57 -0300

    Adição de limite na seleção dos itens e contador

--- js/script.js
+++ js/script.js
@@ -7,52 +7,160 @@ let bebida = document.getElementsByClassName('bebida')
 let sobremesa = document.getElementsByClassName('sobremesa')
 //Icone
 let icone = document.getElementsByTagName('ion-icon')
-
+contador = 0
 //Adicionando eventos
 //Pratos
 prato[0].onclick = function selecionar() {
-  prato[0].classList.toggle('ativo')
-  icone[0].classList.toggle('check')
+  for (let i = 0; i < prato.length; i++) {
+    if (
+      prato[i].classList.contains('ativo') &&
+      icone[i].classList.contains('check')
+    ) {
+      prato[i].classList.remove('ativo')
+      icone[i].classList.remove('check')
+      contador--
+    }
+  }
+  prato[0].classList.add('ativo')
+  icone[0].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 prato[1].onclick = function selecionar() {
-  prato[1].classList.toggle('ativo')
-  icone[1].classList.toggle('check')
+  for (let i = 0; i < prato.length; i++) {
+    if (
+      prato[i].classList.contains('ativo') &&
+      icone[i].classList.contains('check')
+    ) {
+      prato[i].classList.remove('ativo')
+      icone[i].classList.remove('check')
+      contador--
+    }
+  }
+  prato[1].classList.add('ativo')
+  icone[1].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 prato[2].onclick = function selecionar() {
-  prato[2].classList.toggle('ativo')
-  icone[2].classList.toggle('check')
+  for (let i = 0; i < prato.length; i++) {
+    if (
+      prato[i].classList.contains('ativo') &&
+      icone[i].classList.contains('check')
+    ) {
+      prato[i].classList.remove('ativo')
+      icone[i].classList.remove('check')
+      contador--
+    }
+  }
+  prato[2].classList.add('ativo')
+  icone[2].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 //Bebidas
 bebida[0].onclick = function selecionar() {
-  bebida[0].classList.toggle('ativo')
-  icone[3].classList.toggle('check')
+  for (let i = 0; i < bebida.length; i++) {
+    if (
+      bebida[i].classList.contains('ativo') &&
+      icone[i + 3].classList.contains('check')
+    ) {
+      bebida[i].classList.remove('ativo')
+      icone[i + 3].classList.remove('check')
+      contador--
+    }
+  }
+  bebida[0].classList.add('ativo')
+  icone[3].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 bebida[1].onclick = function selecionar() {
-  bebida[1].classList.toggle('ativo')
-  icone[4].classList.toggle('check')
+  for (let i = 0; i < bebida.length; i++) {
+    if (
+      bebida[i].classList.contains('ativo') &&
+      icone[i + 3].classList.contains('check')
+    ) {
+      bebida[i].classList.remove('ativo')
+      icone[i + 3].classList.remove('check')
+      contador--
+    }
+  }
+  bebida[1].classList.add('ativo')
+  icone[4].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 bebida[2].onclick = function selecionar() {
-  bebida[2].classList.toggle('ativo')
-  icone[5].classList.toggle('check')
+  for (let i = 0; i < bebida.length; i++) {
+    if (
+      bebida[i].classList.contains('ativo') &&
+      icone[i + 3].classList.contains('check')
+    ) {
+      bebida[i].classList.remove('ativo')
+      icone[i + 3].classList.remove('check')
+      contador--
+    }
+  }
+  bebida[2].classList.add('ativo')
+  icone[5].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 //Sobremesas
 sobremesa[0].onclick = function selecionar() {
-  sobremesa[0].classList.toggle('ativo')
-  icone[6].classList.toggle('check')
+  for (let i = 0; i < sobremesa.length; i++) {
+    if (
+      sobremesa[i].classList.contains('ativo') &&
+      icone[i + 6].classList.contains('check')
+    ) {
+      sobremesa[i].classList.remove('ativo')
+      icone[i + 6].classList.remove('check')
+      contador--
+    }
+  }
+  sobremesa[0].classList.add('ativo')
+  icone[6].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 sobremesa[1].onclick = function selecionar() {
-  sobremesa[1].classList.toggle('ativo')
-  icone[7].classList.toggle('check')
+  for (let i = 0; i < sobremesa.length; i++) {
+    if (
+      sobremesa[i].classList.contains('ativo') &&
+      icone[i + 6].classList.contains('check')
+    ) {
+      sobremesa[i].classList.remove('ativo')
+      icone[i + 6].classList.remove('check')
+      contador--
+    }
+  }
+  sobremesa[0].classList.add('ativo')
+  icone[7].classList.add('check')
+  contador++
+  console.log(contador)
 }
 
 sobremesa[2].onclick = function selecionar() {
-  sobremesa[2].classList.toggle('ativo')
-  icone[8].classList.toggle('check')
+  for (let i = 0; i < sobremesa.length; i++) {
+    if (
+      sobremesa[i].classList.contains('ativo') &&
+      icone[i + 6].classList.contains('check')
+    ) {
+      sobremesa[i].classList.remove('ativo')
+      icone[i + 6].classList.remove('check')
+      contador--
+    }
+  }
+  sobremesa[2].classList.add('ativo')
+  icone[8].classList.add('check')
+  contador++
+  console.log(contador)
 }
